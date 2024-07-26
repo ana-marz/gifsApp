b7f5d716e9ea7d4fb3970282d909565114221e85
ana-marz <amluquin@hotmail.es>
2024-07-26 17:41:56 +0200

    peticion http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient  } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,10 @@ import { SharedModule } from './shared/shared.module';
     GifsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
